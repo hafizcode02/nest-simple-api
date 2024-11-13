e4cae9eb6a3f2a943b060a24542bdd6730bcf01d
Hafiz Caniago <hafizcode02@gmail.com>
2024-11-13 14:06:53 +0700

    feat: add nest limiter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,29 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
 import { UserModule } from './user/user.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [CommonModule, UserModule],
+  imports: [
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          name: 'global',
+          limit: 100,
+          ttl: 60000,
+        },
+      ],
+    }),
+    CommonModule,
+    UserModule,
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: 'APP_GUARD',
+      useClass: ThrottlerGuard,
+    },
+  ],
   exports: [],
 })
 export class AppModule {}
