0680a90d6d650bcc29b695353be56ef02843931a
Hafiz Caniago <hafizcode02@gmail.com>
2024-11-18 04:02:28 +0700

    feat: update schema prisma

--- prisma/migrations/20241113025800_create_table_contacts/migration.sql
+++ prisma/migrations/20241113025800_create_table_contacts/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE `contacts` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `first_name` VARCHAR(100) NOT NULL,
-    `last_name` VARCHAR(100) NOT NULL,
+    `last_name` VARCHAR(100) NULL,
     `email` VARCHAR(100) NOT NULL,
     `phone` TEXT NOT NULL,
     `social_linkedin` VARCHAR(100) NULL,
