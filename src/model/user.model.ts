c564017107824aa894be0e3e83b13d0971f6d4cb
oreyon <dontrichpeopledifficulut@gmail.com>
2024-11-20 16:14:30 +0800

    setup swagger, add api property in controller and dto

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -1,8 +1,44 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class RegisterUserRequest {
+  @ApiProperty({
+    type: String,
+    minimum: 3,
+    description: 'The name of the user',
+    example: 'example',
+  })
   name: string;
+
+  @ApiProperty({
+    type: String,
+    minimum: 3,
+    description: 'The email of the user',
+    example: 'example@example.com',
+  })
   email: string;
+
+  @ApiProperty({
+    type: String,
+    minimum: 3,
+    description: 'The username of the user',
+    example: 'example',
+  })
   username: string;
+
+  @ApiProperty({
+    type: String,
+    minimum: 8,
+    description: 'The password of the user',
+    example: 'example',
+  })
   password: string;
+
+  @ApiProperty({
+    type: String,
+    minimum: 8,
+    description: 'The password confirmation of the user',
+    example: 'example',
+  })
   confirmPassword?: string;
 }
 
@@ -15,12 +51,35 @@ export class UserResponse {
 }
 
 export class LoginUserRequest {
+  @ApiProperty({
+    type: String,
+    example: 'example',
+  })
   username: string;
+
+  @ApiProperty({
+    type: String,
+    example: 'example',
+  })
   password: string;
 }
 
 export class UpdateUserRequest {
+  @ApiProperty({
+    type: String,
+    example: 'new example',
+  })
   name?: string;
+
+  @ApiProperty({
+    type: String,
+    example: 'new example',
+  })
   password?: string;
+
+  @ApiProperty({
+    type: String,
+    example: 'new example',
+  })
   confirmPassword?: string;
 }
