f374cf7119e98eb7baa8545465c3d0825dc1c2bb
Hafiz Caniago <hafizcode02@gmail.com>
2024-11-21 08:56:47 +0700

    feat: update validation.ts

--- src/address/address.validation.ts
+++ src/address/address.validation.ts
@@ -12,12 +12,14 @@ export class AddressValidation {
     detail: z.string().min(3).max(255).optional(),
   });
 
-  static readonly UPDATE: ZodType = z.object({
-    street: z.string().min(3).max(100).optional(),
-    city: z.string().min(3).max(100).optional(),
-    province: z.string().min(3).max(100).optional(),
-    country: z.string().min(3).max(100).optional(),
-    postalCode: z.string().min(3).max(100).optional(),
-    detail: z.string().min(3).max(255).optional(),
-  });
+  static readonly UPDATE: ZodType = z
+    .object({
+      street: z.string().min(3).max(100),
+      city: z.string().min(3).max(100),
+      province: z.string().min(3).max(100),
+      country: z.string().min(3).max(100),
+      postalCode: z.string().min(3).max(100),
+      detail: z.string().min(3).max(255),
+    })
+    .partial();
 }
--- src/contact/contact.validation.ts
+++ src/contact/contact.validation.ts
@@ -16,18 +16,20 @@ export class ContactValidation {
     social_github: z.string().max(100).optional(),
   });
 
-  static readonly UPDATE: ZodType = z.object({
-    first_name: z.string().min(3).max(100).optional(),
-    last_name: z.string().max(100).optional(),
-    email: z.string().email().min(10).max(100).optional(),
-    phone: z.string().min(8).max(20).optional(),
-    social_linkedin: z.string().max(100).optional(),
-    social_fb: z.string().max(100).optional(),
-    social_x: z.string().max(100).optional(),
-    social_yt: z.string().max(100).optional(),
-    social_ig: z.string().max(100).optional(),
-    social_github: z.string().max(100).optional(),
-  });
+  static readonly UPDATE: ZodType = z
+    .object({
+      first_name: z.string().min(3).max(100),
+      last_name: z.string().max(100),
+      email: z.string().email().min(10).max(100),
+      phone: z.string().min(8).max(20),
+      social_linkedin: z.string().max(100),
+      social_fb: z.string().max(100),
+      social_x: z.string().max(100),
+      social_yt: z.string().max(100),
+      social_ig: z.string().max(100),
+      social_github: z.string().max(100),
+    })
+    .partial();
 
   static readonly SEARCH: ZodType = z.object({
     name: z.string().min(1).optional(),
